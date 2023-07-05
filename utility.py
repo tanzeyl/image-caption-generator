b00866cfd89657cb4b86008df2514ab75bca15c5
Tanzeel <ktanzeel80@gmail.com>
2023-07-06 00:05:53 +0530

    Added all files

---
+++ utility.py
@@ -0,0 +1,52 @@
+from pickle import load
+from numpy import argmax
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.applications.vgg16 import preprocess_input
+from tensorflow.keras.models import Model
+from keras.models import load_model
+
+def word_for_id(integer, tokenizer):
+    for word, index in tokenizer.word_index.items():
+        if index == integer:
+            return word
+    return None
+
+def generate_desc(model, tokenizer, photo, max_length):
+    in_text = 'startseq'
+    for i in range(max_length):
+        sequence = tokenizer.texts_to_sequences([in_text])[0]
+        sequence = pad_sequences([sequence], maxlen=max_length)
+        yhat = model.predict([photo,sequence], verbose=0)
+        yhat = argmax(yhat)
+        word = word_for_id(yhat, tokenizer)
+        if word is None:
+            break
+        in_text += ' ' + word
+        if word == 'endseq':
+            break
+    return in_text
+
+def extract_feature(filename):
+    model = VGG16()
+    model.layers.pop()
+    model = Model(inputs=model.inputs, outputs=model.layers[-1].output)
+    image = load_img(filename, target_size=(224, 224))
+    image = img_to_array(image)
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+    image = preprocess_input(image)
+    feature = model.predict(image, verbose=0)
+    return feature
+
+def caption(test):
+    tokenizer = load(open('./model/tokenizer.pkl', 'rb'))
+    model = load_model('./model/model.h5')
+    max_length = 34
+    photo = extract_feature(test)
+    description = generate_desc(model, tokenizer, photo, max_length)
+    d = description.split(" ")
+    d = d[1:-1]
+    description = " ".join(d)
+    return description.title()
